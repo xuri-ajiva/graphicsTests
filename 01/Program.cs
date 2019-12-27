ff884dcfec98629a806794ce6a852922d268600c
xuri <T0Bl4S.durst@gmail.com>
2019-12-27 15:21:00 +0100

    .

--- 01/Program.cs
+++ 01/Program.cs
@@ -23,7 +23,11 @@ namespace _01 {
 
 
     internal class MainClass : OpenTKFramework.MainClass {
-        int[] array = new int[100];
+        public const int           S       = 1000;
+        private      string        sortere = "null";
+        public       List<ISorter> _sorts  = new List<ISorter>();
+
+        int[] array = new int[S];
 
 
         void flip(int p1, int p2) {
@@ -46,20 +50,140 @@ namespace _01 {
 
                 Thread.Sleep( 1000 );
 
-                for ( int write = 0; write < this.array.Length; write++ ) {
-                    for ( int sort = 0; sort < this.array.Length - 1; sort++ ) {
-                        Thread.Sleep( 1 );
-                        if ( array[sort] > array[sort + 1] ) {
-                            var temp = array[sort + 1];
-                            array[sort + 1] = array[sort];
-                            array[sort]     = temp;
-                        }
-                    }
-                }
+                var s = this.r.Next( this._sorts.Count );
+                this.sortere = _sorts[s].ToString();
+                this._sorts[s].sort();
+
+                //new bubble() { array = this.array }.sort();
+
+                //new merge() { array = this.array }.sort();
+
+                //new heap() { array = this.array }.sort();
 
                 Thread.Sleep( 1000 );
             }
         }
+
+
+        public MainClass() {
+            //this._sorts.Add( new bubble() );
+            this._sorts.Add( new heap() );
+            //this._sorts.Add( new quick() );
+
+            foreach ( var sort in this._sorts ) {
+                sort.array = this.array;
+            }
+
+            for ( int i = 0; i < this.array.Length; i++ ) {
+                this.array[i] = i;
+            }
+
+            Create( new Size( 2000, 200 ) );
+
+            this.Window.VSync = VSyncMode.Off;
+
+            this.Window.TargetRenderFrequency = 100;
+            this.Window.TargetUpdateFrequency = 100;
+
+            this.Window.Closing += delegate(object sender, CancelEventArgs args) { Environment.Exit( 0 ); };
+
+            this.Window.Location = new Point( 300, 100 );
+
+            new Thread( sorter ).Start();
+            Run();
+        }
+
+        #region Overrides of MainClass
+
+        /// <inheritdoc />
+        public override void Initialize(object sender, EventArgs e) { }
+
+        /// <inheritdoc />
+        public override void Shutdown(object sender, EventArgs e) { }
+
+        public override void Render(object sender, FrameEventArgs e) {
+            this.I.ClearScreen( Color.Black );
+
+            var h = this.Window.ClientSize.Height;
+            var w = this.Window.ClientSize.Width;
+            var s = (float) w / (float) this.array.Length;
+
+            var multi = (float) h / (float) w;
+
+            //h = (int) ( h * multi );
+
+            for ( int i = 0; i < this.array.Length; i++ ) {
+                this.I.DrawRect( new RectangleF( (float) i * s, (float) h, (float) s, (float) -this.array[i] * multi * s ), fromvaule( i, this.array[i], ( 1 / (float) this.array.Length ) * 5 ) );
+            }
+
+            this.I.DrawString( sortere, new PointF( 10, this.Window.ClientSize.Height - 20 ), Color.BlueViolet );
+        }
+
+        Color fromvaule(int i, int h, double v) {
+            var a = v;
+            return Color.FromArgb( NSin( h * a, Math.Sin ), NSin( h * a, Math.Cos ), NSin( h * a, d => -Math.Sin( d ) ) );
+        }
+
+        int NSin(double i, Func<double, double> mathFunc) { return (int) ( ( mathFunc( i ) + 1D ) / 2D * 255D ); }
+
+        public override void Update(object sender, FrameEventArgs e) { }
+
+        #endregion
+    }
+
+    class heap : ISorter {
+        private const int S = MainClass.S;
+
+        #region Implementation of ISorter
+
+        /// <inheritdoc />
+        public int[] array { get; set; }
+
+        /// <inheritdoc />
+        public void sort() { heapSort( array, array.Length ); }
+
+        static void heapSort(int[] arr, int n) {
+            for ( int i = n / 2 - 1; i >= 0; i-- ) {
+                Thread.Sleep( (int) ( 1000F / S ) );
+                heapify( arr, n, i );
+            }
+
+            for ( int i = n - 1; i >= 0; i-- ) {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Thread.Sleep( (int) ( 1000F / S ) );
+                heapify( arr, i, 0 );
+            }
+        }
+
+        static void heapify(int[] arr, int n, int i) {
+            int largest                                           = i;
+            int left                                              = 2 * i + 1;
+            int right                                             = 2 * i + 2;
+            if ( left  < n && arr[left]  > arr[largest] ) largest = left;
+            if ( right < n && arr[right] > arr[largest] ) largest = right;
+            if ( largest != i ) {
+                int swap = arr[i];
+                arr[i]       = arr[largest];
+                arr[largest] = swap;
+                heapify( arr, n, largest );
+            }
+        }
+
+        #endregion
+    }
+
+    class merge : ISorter {
+        #region Implementation of ISorter
+
+        /// <inheritdoc />
+        public int[] array { get; set; }
+
+        /// <inheritdoc />
+        public void sort() { array = MergeSort( array.ToList() ).ToArray(); }
+
 
         private static List<int> MergeSort(List<int> unsorted) {
             if ( unsorted.Count <= 1 ) return unsorted;
@@ -111,59 +235,89 @@ namespace _01 {
             return result;
         }
 
-        public MainClass() {
-            for ( int i = 0; i < this.array.Length; i++ ) {
-                this.array[i] = i;
-            }
+        #endregion
+    }
 
-            Create(new Size(1700,1000));
-
-            this.Window.VSync = VSyncMode.Off;
-
-            this.Window.TargetRenderFrequency = 100;
-            this.Window.TargetUpdateFrequency = 100;
-
-            this.Window.Closing += delegate(object sender, CancelEventArgs args) { Environment.Exit( 0 ); };
-            
-            this.Window.Location = new Point(300,100);
-
-            new Thread( sorter ).Start();
-            Run();
-        }
-
-        #region Overrides of MainClass
+    class bubble : ISorter {
+        private const int S = MainClass.S;
+        
+        #region Implementation of ISorter
 
         /// <inheritdoc />
-        public override void Initialize(object sender, EventArgs e) { }
+        public int[] array { get; set; }
 
         /// <inheritdoc />
-        public override void Shutdown(object sender, EventArgs e) { }
-
-        public override void Render(object sender, FrameEventArgs e) {
-            this.I.ClearScreen( Color.Black );
-
-            var h = this.Window.ClientSize.Height;
-            var w = this.Window.ClientSize.Width;
-            var s = w / this.array.Length;
-
-            var multi = (float) h / w;
-
-            //h = (int) ( h * multi );
-
-            for ( int i = 0; i < this.array.Length; i++ ) {
-                this.I.DrawRect( new RectangleF( (float) i * s, (float) h, (float) s, (float) -this.array[i] * multi * s ), fromvaule( i, this.array[i] ,w) );
+        public void sort() {
+            for ( int write = 0; write < this.array.Length; write++ ) {
+                Thread.Sleep( (int) ( 1000F / S ) );
+                for ( int sort = 0; sort < this.array.Length - 1; sort++ ) {
+                    if ( array[sort] > array[sort + 1] ) {
+                        var temp = array[sort + 1];
+                        array[sort + 1] = array[sort];
+                        array[sort]     = temp;
+                    }
+                }
             }
-
-            //this.I.DrawString( base.totalFramesUpdated.ToString(), new PointF( 10, this.Window.ClientSize.Height - 20 ), Color.BlueViolet );
         }
-
-        Color fromvaule(int i, int h, double v) {
-            var a = this.array.Length/v; return Color.FromArgb( NSin( h*a, Math.Sin ), NSin( h*a, Math.Cos ), NSin( h*a, d => -Math.Sin( d ) ) ); }
-
-        int NSin(double i, Func<double, double> mathFunc) { return (int) ( ( mathFunc( i ) + 1D ) / 2D * 255D ); }
-
-        public override void Update(object sender, FrameEventArgs e) { }
 
         #endregion
+    }
+
+    class quick : ISorter {
+        
+        private const int S = MainClass.S;
+
+        private int[] _array;
+
+        private static void quicksort(int links, int rechts, ref int[] daten) {
+            if ( links < rechts ) {
+                int teiler = teile( links, rechts, ref daten );
+                quicksort( links, teiler - 1, ref daten );
+                quicksort( teiler        + 1, rechts, ref daten );
+            }
+        }
+
+        private static int teile(int links, int rechts, ref int[] daten) {
+            int i = links;
+            int j     = rechts - 1;
+            int pivot = daten[rechts];
+
+            do {
+                while ( daten[i] <= pivot && i < rechts ) i += 1;
+                while ( daten[j] >= pivot && j > links ) j -= 1;
+
+                
+                Thread.Sleep( (int) ( 1000F / S ) );
+
+                if ( i < j ) {
+                    int z = daten[i];
+                    daten[i] = daten[j];
+                    daten[j] = z;
+                }
+            } while ( i < j );
+
+            if ( daten[i] > pivot ) {
+                int z = daten[i];
+                daten[i] = daten[rechts];
+                daten[rechts] = z;
+            }
+
+            return i;
+        }
+
+        #region Implementation of ISorter
+
+        /// <inheritdoc />
+        public int[] array { get => this._array; set => this._array = value; }
+
+        /// <inheritdoc />
+        public void sort() { quicksort( 0, _array.Length - 1, ref this._array ); }
+
+        #endregion
+    }
+
+    interface ISorter {
+        int[] array { get; set; }
+        void  sort();
     }
 }
