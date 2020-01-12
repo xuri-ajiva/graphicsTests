a2886799db97d3d2d2d00831fa85c91487d5c57c
xuri <T0Bl4S.durst@gmail.com>
2020-01-12 18:53:07 +0100

    3 ands improved renderer

--- 02/Program.cs
+++ 02/Program.cs
@@ -19,7 +19,10 @@ namespace _02 {
         private static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault( false );
-            new MainClass();
+
+            while ( true ) {
+                new MainClass();
+            }
         }
     }
 
@@ -36,13 +39,13 @@ namespace _02 {
 
         private Queue<Paket> _update = new Queue<Paket>();
 
-        private const int COLORS_I     = 10;
+        private const int COLORS_I     = 40;
         private const int COLOR_STEP_I = 240 / COLORS_I;
         private const int COLOR_HALF   = 255 / 2;
 
-        private       float   scale   = 1;
-        private       float   PS      = 2;
-        private const int     maxSize = 10000;
+        private       float   scale   = .05F;
+        private       float   PS      = 20;
+        private const int     maxSize = 20000;
         private       byte[,] areal   = new byte[maxSize, maxSize];
         private       int     w, h, w2, h2, ox, oy;
 
@@ -58,7 +61,6 @@ namespace _02 {
         /// <inheritdoc />
         public override void Render(object sender, FrameEventArgs e) {
             this.I.ClearScreen( Color.FromArgb( 1, 82, 82, 82 ) );
-            SizeF s = new SizeF( PS *this.scale, PS *this.scale  );
             this.Window.Title = "     ArraySize: " + maxSize + "     Position: {x: " + ox + ",y: " + oy + "}     FPS: " + this.frameRate + "     Scale: " + this.scale + "     PixelSize: " + this.PS;
 
             //while ( this._update.Count > 0 ) {
@@ -100,13 +102,11 @@ namespace _02 {
             //    }
             //}
 
-            for ( float i = 0; i < maxSize; i += s.Width ) {
-                for ( float j = 0; j < maxSize; j += s.Height ) {
+            //SizeF s = new SizeF( PS, PS);
+            for ( float i = 0; i < maxSize; i += PS ) {
+                for ( float j = 0; j < maxSize; j += PS ) {
                     var x = i + this.ox;
                     var y = j + this.oy;
-
-                    if ( this.scale * x < 0  || this.scale * y < 0 ) continue;
-                    if ( this.scale * x >= w || this.scale * y >= h ) continue;
 
                     if ( x < 0        || y < 0 ) continue;
                     if ( x >= maxSize || y >= maxSize ) continue;
@@ -115,9 +115,13 @@ namespace _02 {
 
                     if ( v == 0 ) continue;
 
-                    var p = new PointF( this.scale * x, this.scale * y );
+                    if ( this.scale * x < 0  || this.scale * y < 0 ) continue;
+                    if ( this.scale * x >= w || this.scale * y >= h ) continue;
 
-                    // if ( Math.Abs( ( (int) p.X - p.X ) ) < .1 || Math.Abs( ( (int) p.Y - p.Y ) ) < .1)
+                    var p = new PointF( this.scale * x, this.scale * y );
+                    var s = new SizeF( this.scale * ( x + this.PS ) - this.scale * x, this.scale * ( y + PS ) - this.scale * y );
+
+                    //if ( Math.Abs( ( (int) p.X - p.X ) ) < .1 || Math.Abs( ( (int) p.Y - p.Y ) ) < .1 )
                     this.I.DrawRect( new RectangleF( p, s ), colorFromValue( v ) );
                     //this.I.DrawRect( new RectangleF( p, new SizeF(1,1) ), colorFromValue( v ) );
                 }
@@ -140,17 +144,6 @@ namespace _02 {
             // return Color.Blue;
         }
 
-        private PointF Map(float x, float y, float s, byte[,] a, int m, int h) {
-            var p = new PointF();
-            x *= this.scale;
-            y *= this.scale;
-
-            p.X = h + x;
-            p.Y = h + y;
-
-            return p;
-        }
-
         /// <inheritdoc />
         public override void Update(object sender, FrameEventArgs e) {
             this.w  = this.Window.ClientSize.Width;
@@ -161,7 +154,11 @@ namespace _02 {
 
         #endregion
 
+        private bool exit = true;
+
         private bool fe = true;
+
+        List<Thread> _ts = new List<Thread>();
 
         public MainClass() {
             for ( int i = 0; i < maxSize; i++ ) {
@@ -170,7 +167,7 @@ namespace _02 {
                 }
             }
 
-            Create( new Size( 800, 500 ) );
+            Create( new Size( 1000, 1000 ) );
             this.Window.Location = new Point( 300, 100 );
 
             //this.Window.WindowState = WindowState.Fullscreen;
@@ -181,6 +178,7 @@ namespace _02 {
 
             this.Window.KeyDown += delegate(object s, KeyboardKeyEventArgs e) {
                 if ( e.Key == Key.Escape ) {
+                    exit = false;
                     this.Window.Close();
                 }
 
@@ -201,7 +199,7 @@ namespace _02 {
                 }
 
                 if ( this.scale <= 0 ) this.scale = 0.05F;
-                if ( this.PS    <= 0 ) this.PS    = .2F;
+                if ( this.PS    <= .01 ) this.PS  = .01F;
             };
             bool down = false;
             this.Window.MouseDown += (sender, args) => down = true;
@@ -210,41 +208,81 @@ namespace _02 {
             this.Window.MouseMove += delegate(object sender, MouseMoveEventArgs args) {
                 if ( !down ) return;
 
-                this.ox += (int)(args.XDelta*this.PS);
-                this.oy += (int)(args.YDelta*this.PS);
+                this.ox += (int) ( args.XDelta * this.PS );
+                this.oy += (int) ( args.YDelta * this.PS );
             };
 
-            this.Window.Closing += delegate { Environment.Exit( 0 ); };
-            this.w              =  this.Window.ClientSize.Width;
-            this.h              =  this.Window.ClientSize.Height;
-            this.h2             =  this.Window.ClientSize.Height / 2;
-            this.w2             =  this.Window.ClientSize.Width  / 2;
+            this.Window.Closing += delegate {
+                if ( this.exit ) Environment.Exit( 0 );
+                else
+                { foreach ( var t in this._ts )
+                        t.Abort();
+                    this.areal = null;
+                    GC.Collect();
+                }
+            };
+            this.w  = this.Window.ClientSize.Width;
+            this.h  = this.Window.ClientSize.Height;
+            this.h2 = this.Window.ClientSize.Height / 2;
+            this.w2 = this.Window.ClientSize.Width  / 2;
 
-            this.mv.AddRange( Enumerable.Repeat( false, COLORS_I ) );
+            this.mv.AddRange( Enumerable.Repeat( false, COLORS_I+10 ) );
 
             Random r = new Random();
 
-            //for ( int i = 0; i < COLORS_I / 2; i++ ) {
-            //    var v = r.NextDouble() > .5;
-            //    Thread.Sleep( 1 );
-            //    this.mv[r.Next( 0, COLORS_I )] = v;
-            //    Thread.Sleep( 1 );
-            //}
-            this.mv[2] = true;
+            for ( int i = 0; i < COLORS_I / 4; i++ ) {
+                var v = r.NextDouble() > .5;
+                Thread.Sleep( 1 );
+                this.mv[r.Next( 0, COLORS_I )] = v;
+                Thread.Sleep( 1 );
+            }
+            //this.mv[2] = true;
+            //this.mv[8] = true;
+
+            //this.mv[13] = true;
+            //this.mv[21] = true;
+            //this.mv[39] = true;
+            //this.mv[40] = true;
+            //this.mv[41] = true;
+            //this.mv[75] = true;
+            //this.mv[77] = true;
+            //this.mv[78] = true;
+            //this.mv[89] = true;
+
+            //this.mv[4] = true;
+            //this.mv[9] = true;
+            //this.mv[10] = true;
+            //this.mv[24] = true;
+            //this.mv[30] = true;
+            //this.mv[41] = true;
+            //this.mv[52] = true;
+            //this.mv[61] = true;
+            //this.mv[64] = true;
+            //this.mv[65] = true;
+            //this.mv[70] = true;
+            //this.mv[74] = true;
+            //this.mv[78] = true;
+            //this.mv[81] = true;
 
             //for ( int i = 0; i < 1; i++ ) {
             //    this._creeps.Add( new Creep( Direction.n, new Point( ( maxSize / 2 ) + (int) ( i * r.NextDouble() * 100 ), ( maxSize / 2 ) + (int) ( i * r.NextDouble() * 100 ) ) ) );
             //}
 
-            this._creeps.Add( new Creep( Direction.n, new Point( 100, 100 ) ) );
+            this._creeps.Add( new Creep( Direction.n, new Point( maxSize     / 4, maxSize     / 4 ) ) );
+            this._creeps.Add( new Creep( Direction.n, new Point( maxSize / 4 * 2, maxSize / 4 * 2 ) ) );
+            this._creeps.Add( new Creep( Direction.n, new Point( maxSize / 4 * 3, maxSize / 4 * 3 ) ) );
 
             Console.WriteLine( "setupDirections" );
 
-            for ( var i = 0; i < this.mv.Count; i++ ) {
-                Console.WriteLine( "[" + i + "]: " + ( this.mv[i] ? "R" : "L" ) );
+            for ( var i = 0; i < COLORS_I; i++ ) {
+                Console.WriteLine( "mv[" + i + "] = "+this.mv[i]+";           /// # " + ( this.mv[i] ? "R" : "L" ) );
             }
 
-            new Thread( Work ).Start();
+            foreach ( var c in this._creeps ) {
+                var t = new Thread( () => Work( c ) );
+                t.Start();
+                this._ts.Add( t );
+            }
 
             Run();
         }
@@ -252,15 +290,13 @@ namespace _02 {
         private List<Creep> _creeps = new List<Creep>();
 
 
-        private void Work() {
+        private void Work(Creep c) {
             while ( true ) {
-                foreach ( var c in this._creeps ) {
-                    if ( c.pos.X < 0 ) c.pos.X        = 1;
-                    if ( c.pos.Y < 0 ) c.pos.Y        = 1;
-                    if ( c.pos.Y >= maxSize ) c.pos.Y = maxSize - 2;
-                    if ( c.pos.X >= maxSize ) c.pos.X = maxSize - 2;
-                    WorkDirection( this.areal[c.pos.X, c.pos.Y], c );
-                }
+                if ( c.pos.X < 0 ) c.pos.X        = 1;
+                if ( c.pos.Y < 0 ) c.pos.Y        = 1;
+                if ( c.pos.Y >= maxSize ) c.pos.Y = maxSize - 2;
+                if ( c.pos.X >= maxSize ) c.pos.X = maxSize - 2;
+                WorkDirection( this.areal[c.pos.X, c.pos.Y], c );
 
                 //Thread.Sleep( 1 );
             }
@@ -273,7 +309,7 @@ namespace _02 {
 
             bool dx = this.mv[value];
 
-            if ( value == COLORS_I - 1 ) {
+            if ( value >= COLORS_I - 1 ) {
                 dx                           = true;
                 icrese                       = false;
                 this.areal[c.pos.X, c.pos.Y] = 0;
